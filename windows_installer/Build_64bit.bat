41a1bec4105279c2d46c7266cc9d12d154e715eb
andrew <admin@AC-HOME>
2021-07-28 11:49:06 +1000

    Build batch files now default to require admin rights.

--- windows_installer/Build_64bit.bat
+++ windows_installer/Build_64bit.bat
@@ -1,2 +1,3 @@
+"C:\Program Files (x86)\NSIS\makensis.exe" "/DBUILD_TYPE=64" "/DNIGHTLY_BUILD_SVN=12487" "/DCB_ADMIN_INSTALLER=True" "Installer_NSIS_x64.nsi"
 "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" "/DBUILD_TYPE=64" "/DNIGHTLY_BUILD_SVN=12487" "/DCB_ADMIN_INSTALLER=True" "Installer_ISS_x64 - Full Only.iss"
 pause
