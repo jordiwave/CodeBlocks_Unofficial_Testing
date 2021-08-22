622513a5c74f504d999a0ca819335f53893e9f59
AndrweCottrell <andnws@exemail.com.au>
2021-08-22 17:05:27 +1000

    Update batch files.

--- windows_installer/Build_ISS_32bit.bat
+++ windows_installer/Build_ISS_32bit.bat
@@ -1,2 +1,2 @@
-"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" "/DBUILD_TYPE=32" "/DNIGHTLY_BUILD_SVN=12492" "Installer_ISS_full_only.iss"
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" "/DBUILD_TYPE=32" "/DNIGHTLY_BUILD_SVN=12516" "Installer_ISS_full_only.iss"
 pause
